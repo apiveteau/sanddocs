35124b74d9c67eb791dc6660d70a8f9b55aa6e8a
Anatole Piveteau <anatole.piveteau@gmail.com>
2021-09-06 12:22:21 +0200

    Add Jquery Can collapse menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,8 @@ $('aside>.burger').on('click', (e) => {
     e.preventDefault();
     $('body').toggleClass('menu-close')
 })
+$('aside>nav .collapser').on('click', (e) => {
+    $(e.target).toggleClass('active');
+    $(e.target).parent().next().toggleClass('open');
+    e.preventDefault();
+})
